77bb54bebe8f791d1dfd48bbec30f4456fbd2c13
Ivan Orlov <nchanged@gmail.com>
2015-02-21 20:27:50 +0200

    Without limit

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -16,9 +16,11 @@ module.exports = function(req, res)
                 }
             });
         }
-        movies.limit(50);
+        //movies.limit(50);
         movies.order({age : 'asc'})
         movies.all(function(movieList){
+
+
         	res.render('index', {genres : genres, movies : movieList, currentGenre : currentGenre});
         });
 		
