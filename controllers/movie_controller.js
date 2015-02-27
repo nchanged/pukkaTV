b2de22eb52444c23b3619b6562ed1835773eee88
Ivan Orlov <nchanged@gmail.com>
2015-02-27 22:07:00 +0200

    Yet another fix

--- controllers/movie_controller.js
+++ controllers/movie_controller.js
@@ -1,4 +1,5 @@
 var models = require('../models');
+var _ = require('lodash');
 module.exports = function(req, res) {
     var movie_id = req.params.id;
     new models.Genre().find().order({
@@ -10,13 +11,33 @@ module.exports = function(req, res) {
         }).first(function(movie) {
             if (movie) {
 
+
                 new models.MovieMagnet().find({
                     movie_id: movie_id
                 }).all(function(magnets) {
+                    var priorityMagnets = [];
+                    var restMagnets = [];
+                     _.each(magnets, function(magnet) {
+                        var quality = magnet.get('quality')
+                        var title = magnet.get('full_title');
+                        var shitQuality = quality.match(/CAM|TeleSync|TS|Telecine|Screener|Unknown/i)
+                        var isGood = (title.match(/\sHD/i) && !shitQuality) || (!title.match(/cam/i) && 
+                            quality.match(/720p|1080p|HDRIP|DVDRip/i))
+                        
+                        if ( isGood && !shitQuality){
+                            priorityMagnets.push(magnet)
+                        } else {
+                            restMagnets.push(magnet);
+                        }
+                        
+                    });
+                     var sortedMagnets = priorityMagnets.concat(restMagnets)
+                     
+                     
                     res.render('movie_details', {
                         genres: genres,
                         movie: movie,
-                        magnets : magnets
+                        magnets: sortedMagnets
                     });
                 })
 
